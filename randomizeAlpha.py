c6c1d30303106f4c64df34aae4977d08534eae57
Barry F Smith <21973169+bfsmith9@users.noreply.github.com>
2018-10-22 17:47:37 -0400

    Update randomizeAlpha.py
    Trying to figure out match between regular alphabet and randomized alphabet.

--- randomizeAlpha.py
+++ randomizeAlpha.py
@@ -2,18 +2,39 @@
 
 #########################################################
 # ASSIGNMENT 5 - CS166                                  #
-# PROGRAM TO MANAGE PASSWORDS & ACCESS LEVELS FOR USERS #
-# BARRY SMITH - SEPT 11, 2018                           #
+# SMALL "ONE-TIME" PROGRAM TO RANDOMIZE LETTERS         #
+# BARRY SMITH - OCT 23, 2018                            #
 #########################################################
 
 
 
 import random
 
-def encryptPassword(pw):
-    print("Here is your password" + pw)
-    alphabet = "abcdefghijklmnopqrstuvwxyz"
-    mixed = list(alphabet)
-    random.shuffle(mixed)
+def randomizeAlphabet():
+    alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWX0123456789"
+    regAlphaList = list(alphabet)
+    alphaList = list(alphabet)
+    random.shuffle(alphaList)
+    mixed = alphaList
+  
+    # Kind of weird to me. Does this just turn a list into string?
+    print(*mixed, sep=',')
+    print(mixed.index('h'))
+    numVal = mixed.index('h')
+    print(numVal)
+    for x in mixed:
+        #
+        print(x)
+        print(mixed.index(x))
+        #print(alphaList.index(x))
+        #print(mixed[x])
+        print(regAlphaList[mixed.index(x)])
+        print()
+    # print(alphabet[numVal])      
     mixed = ''.join(mixed)
     print(mixed)
+    
+    
+    
+    
+randomizeAlphabet()
