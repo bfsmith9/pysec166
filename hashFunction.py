57b7e7651d03a87d707fcf97255ffd4dae37a279
Barry F Smith <21973169+bfsmith9@users.noreply.github.com>
2018-11-02 12:39:04 -0400

    Update hashFunction.py
    Hash function with salt is working.

--- hashFunction.py
+++ hashFunction.py
@@ -9,7 +9,21 @@
 import hashlib
 
 def hashPassword(password, salt):
+    print(password)
+    print(salt)
     hash = hashlib.sha512(password.encode()).hexdigest()
+    print(hash)
     hashedSalt = hashlib.sha512(salt.encode()).hexdigest()
+    print(hashedSalt)
+    passwordPlusSalt = hash + hashedSalt
+    print(passwordPlusSalt)
+    hashCombo = hashlib.sha512(passwordPlusSalt.encode()).hexdigest()
+    print(hashCombo)
+    
+    return hashCombo
+
+# This is working
+# hashPassword("dogbert", "remy")
+
     
 
